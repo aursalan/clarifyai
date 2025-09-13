68f0ca21e929301c5573f3a7ffa345c79ec52d21
Aursalan Sayed <aursalansayed@gmail.com>
2025-09-13 09:48:59 +0530

    Update header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full max-w-4xl mx-auto flex items-center justify-start px-4">
+    <div className="absolute bottom-0 w-full max-w-4xl mx-auto flex items-center justify-start px-4">
       <h1 className="font-bold text-2xl bg-gradient-to-r from-cyan-400 via-blue-500 to-indigo-500 bg-clip-text text-transparent">
         ClarifyAI
       </h1>
